018bca525f17e79feebed4358ebc05da0a11f6aa
Bambang Adhitya <bambang@kiosawan.com>
2019-12-11 17:00:11 +0700

    Deeplink

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import AppNavigator from './src/routes/AppNavigator'
 
 import React, { Component } from 'react';
+import { Linking } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
 
-export default class App extends Component {
 
+const prefix = 'mychat://';
+let url = "awanapp://open?a=123&b=32846&c=6847bsdfj"
+
+console.log(url.getParamFromUrl())
+
+export default class App extends Component {
+  componentDidMount() {
+    Linking.getInitialURL().then((url) => {
+      // alert(url)
+    }).catch(err => console.error('An error occurred', err));
+    Linking.addEventListener('url', this._handleOpenURL);
+  }
+  componentWillUnmount() {
+    Linking.removeEventListener('url', this._handleOpenURL);
+  }
+  _handleOpenURL(event) {
+    // alert(event.url)
+    console.log(event.url);
+  }
   render() {
     return (
       <Provider store={store}>
-        <AppNavigator/>
+        <AppNavigator uriPrefix={prefix} />
       </Provider>
     );
   }
