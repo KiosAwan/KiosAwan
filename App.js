503ac9f49533b200cfe1ae96f85f5e05348f26bf
Bambang Adhitya <bambang@kiosawan.com>
2019-12-11 18:10:35 +0700

    Listener Deeplink

--- App.js
+++ App.js
@@ -1,29 +1,23 @@
+import './src/utils/default'
 import AppNavigator from './src/routes/AppNavigator'
-
 import React, { Component } from 'react';
 import { Linking } from 'react-native';
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
+import { Deeplink } from './src/routes/Deeplink';
 
 
-const prefix = 'mychat://';
-let url = "awanapp://open?a=123&b=32846&c=6847bsdfj"
-
-console.log(url.getParamFromUrl())
-
+const prefix = 'awanapp://';
 export default class App extends Component {
   componentDidMount() {
-    Linking.getInitialURL().then((url) => {
-      // alert(url)
-    }).catch(err => console.error('An error occurred', err));
+    Linking.getInitialURL().then(Deeplink).catch(err => console.error('An error occurred', err));
     Linking.addEventListener('url', this._handleOpenURL);
   }
   componentWillUnmount() {
     Linking.removeEventListener('url', this._handleOpenURL);
   }
   _handleOpenURL(event) {
-    // alert(event.url)
-    console.log(event.url);
+    Deeplink(event.url)
   }
   render() {
     return (
---
+++ src/routes/Deeplink/index.js
@@ -0,0 +1,10 @@
+export const Deeplink = url => {
+	if (!url){
+		return false
+	}
+	const urlObj = {
+		url: url.getRawUrl(),
+		params: url.getParamFromUrl()
+	}
+	console.log(urlObj)
+}
