ed384fc53eae321cd1159e6e4d1c8d6cbaf3f3c7
AwanAlbert <albert@kiosawan.com>
2019-12-16 15:40:16 +0700

    1 line hidup dan mati

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -11,7 +11,7 @@ const AppNavigator = createSwitchNavigator({
     '/splashscreen': CheckMember,
     '/intro': AppIntro
   }, {
-    initialRouteName: 'CheckMember'
+    initialRouteName: '/splashscreen'
   })
 
 
