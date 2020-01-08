83753ec6e192077d7297e019d2d1dfa780ae8e47
Bambang Adhitya <bambang@kiosawan.com>
2020-01-08 14:19:38 +0700

    cetak

--- src/components/CustomImage.js
+++ src/components/CustomImage.js
@@ -2,5 +2,7 @@ import React from 'react';
 import { Image as ImageRN } from "react-native"
 
 export const Image = props => {
-	return <ImageRN {...props} style={[{ width: props.size || undefined, height: props.size || undefined }, props.style]} />
+	return <ImageRN {...props} style={[{ width: props.size || null, height: props.size || null }, props.style]} />
 }
+
+export const ImageAuto = props => <ImageRN {...props} style={{ width: null, height: null, flex: 1, ...props.style }} />
