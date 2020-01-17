779c8580b3d85cdf7f012caf9ff527b91c66cee0
AwanAlbert <albert@kiosawan.com>
2020-01-17 15:35:40 +0700

    sdaf

--- src/screens/AuthScreen/Drawer/index.js
+++ src/screens/AuthScreen/Drawer/index.js
@@ -10,6 +10,7 @@ import { Text } from 'src/components/Text/CustomText'
 import { BottomButton } from 'src/components/Button/ButtonComp';
 import { SizeList } from 'src/styles/size';
 import { Image } from 'src/components/CustomImage';
+import { Button } from 'src/components/Button/Button';
 
 const Akun = ({ navigation }) => {
 	const ListMenu = require('src/assets/json/drawer.json')
@@ -35,21 +36,27 @@ const Akun = ({ navigation }) => {
 	return (
 		<View style={{ flex: 1 }}>
 			<GlobalHeader title="Setting" onPressBack={() => navigation.navigate('/')} />
-			<Grid style={{ backgroundColor: ColorsList.authBackground, height: 100 }}>
-				<Row size={.75} style={{ backgroundColor: 'white', padding: 10 }}>
-					<Col style={{ justifyContent: 'center' }} size={1.5}>
-					<Image size={50} source={require('src/assets/icons/profile.png')} />
+			<Grid style={{ backgroundColor: ColorsList.authBackground, height: 170, justifyContent: 'space-between' }}>
+				<Row size={1.2} style={{ backgroundColor: 'white', padding: 10, alignItems: "center" }}>
+					<Col style={{ justifyContent: 'center' }} size={2}>
+						<Image size={50} source={require('src/assets/icons/profile.png')} />
 					</Col>
 					<Col style={{ justifyContent: 'center' }} size={8}>
 						<Grid style={{ justifyContent: 'center' }}>
 							<Row>
-								<Text>{User.data.name}</Text>
+								<Text color="primary">{User.data.name}</Text>
 							</Row>
 							<Row>
 								<Text>{User.store ? User.store.name_store : '~ Belum Ada Toko ~'}</Text>
 							</Row>
+							<Row>
+								<Text>#AWN00000001</Text>
+							</Row>
 						</Grid>
 					</Col>
+					<View style={{borderWidth : 1, padding : 5,borderColor :ColorsList.greyFont,borderRadius : 5}}>
+						<Text>Free User</Text>
+					</View>
 				</Row>
 				<Row size={9}>
 					{
@@ -57,7 +64,7 @@ const Akun = ({ navigation }) => {
 							return (
 								<Col key={i} style={{ padding: '5%' }}>
 									{
-										groupMenu.map((menu, o) => {											
+										groupMenu.map((menu, o) => {
 											return <TouchableOpacity key={o} onPress={() => User.store ? User.data.status == 1 ? navigation.navigate(menu.route) : null : null} style={{
 												backgroundColor: 'white',
 												marginBottom: 5,
@@ -66,7 +73,7 @@ const Akun = ({ navigation }) => {
 												height: 45
 											}}>
 												<Grid>
-													<Col style={{ alignItems: 'center', justifyContent : "center" }} size={1}>
+													<Col style={{ alignItems: 'center', justifyContent: "center" }} size={1}>
 														<Image size={30} source={images[o]} />
 													</Col>
 													<Col style={{ justifyContent: 'center', marginLeft: 10 }} size={8}>
@@ -85,13 +92,10 @@ const Akun = ({ navigation }) => {
 					}
 				</Row>
 			</Grid>
-			<View style={{ position: 'absolute', bottom: 10, alignSelf: "center" }}>
+			<View style={{ position: 'absolute', bottom: 10, alignSelf: "center", width: '90%' }}>
 				<Text style={{ alignSelf: 'center', marginBottom: 10 }}>Versi 1.0.0</Text>
-				<BottomButton
-					onPressBtn={_onPressLogout}
-					style={{ width: SizeList.width - 30, backgroundColor: ColorsList.primaryColor }}
-					buttonTitle="KELUAR"
-				/>
+				<Button width="100%" style={{ marginBottom: 10 }}>UPGRADE KE PREMIUM</Button>
+				<Button color="white" width="100%" onPress={_onPressLogout}>KELUAR</Button>
 			</View>
 		</View>
 	)
