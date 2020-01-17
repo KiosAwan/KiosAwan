fedeb11c70a519425d49d0731944c90ab24598eb
Bambang Adhitya <bambang@kiosawan.com>
2020-01-17 17:48:56 +0700

    Padding

--- src/components/Card/CardIcon.js
+++ src/components/Card/CardIcon.js
@@ -7,7 +7,7 @@ import { SizeList } from 'src/styles/size';
 export const CardIcon = (props) => (
     <TouchableOpacity onPress={props.onPress}>
         <View style={styles.container}>
-            <Image style={{ width: 50, height: 50}} source={props.icon} />
+            <Image style={{ width: 50, height: 50 }} source={props.icon} />
             <Text align="center" size={12}>{props.name}</Text>
         </View>
     </TouchableOpacity>
@@ -16,10 +16,10 @@ export const CardIcon = (props) => (
 const styles = StyleSheet.create({
     container: {
         backgroundColor: ColorsList.whiteColor,
-        padding :20,
-        borderRadius : 5,
-        alignItems : "center",
-        width : SizeList.width /4,
-        height : SizeList.width/4
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+        width: SizeList.width / 4,
+        height: SizeList.width / 4
     }
 })
