e23e66cc4d5fe76a8f29e54e4c23a79f8fa72714
Bambang Adhitya <bambang@kiosawan.com>
2020-03-19 10:15:26 +0700

    afsdfgkjfdsfdgfjk

--- src/screens/AuthScreen/PPOB/Topup.js
+++ src/screens/AuthScreen/PPOB/Topup.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, ScrollView, FlatList, StyleSheet } from 'react-native';
 import { GlobalHeader } from 'src/components/Header/Header';
 import { ColorsList } from 'src/styles/colors';
 import { convertRupiah } from 'src/utils/authhelper';
-import { FloatingInput, InputCurrency } from 'src/components/Input/InputComp';
+import { FloatingInputLabelCurrency } from 'src/components/Input/InputComp';
 import { Button } from 'src/components/Button/Button';
 import { Wrapper } from 'src/components/View/Wrapper';
 import { Text } from 'src/components/Text/CustomText';
@@ -13,10 +13,12 @@ import { Bottom } from 'src/components/View/Bottom';
 import { useSelector } from 'react-redux';
 import { requestTopUp } from 'src/utils/api/ppobapi';
 import { AwanPopup } from 'src/components/ModalContent/Popups';
+import { getPaymentChannelList } from 'src/utils/api/topup_api';
 
 const Topup = ({ navigation }) => {
 	const [topupValue, setTopupValue] = useState(0)
 	const [topupMethod, setTopupMethod] = useState()
+	const [listPaymentMethod, setListPaymentMethod] = useState([])
 	const [apiLoading, setApiLoading] = useState(false)
 	//alert
 	const [alert, setAlert] = useState(false)
@@ -44,11 +46,24 @@ const Topup = ({ navigation }) => {
 		image: require('src/assets/icons/ppob/topup/BRI.png'),
 		key: 800
 	}]
+
+	useEffect(() => {
+		_getPaymentData()
+	}, [])
+
+	// Get payment channel List data
+	const _getPaymentData = async () => {
+		const res = await getPaymentChannelList()
+		setListPaymentMethod(res.data)
+	}
+
 	const _selectTopupMethod = (item, i) => {
-		setTopupMethod(item.key)
+		setTopupMethod(item.id)
 	}
 
 	const _pressAddTopUpValue = (value) => {
+		console.debug("value", value)
+		console.debug(topupValue)
 		if (!topupValue) {
 			setTopupValue(value)
 		} else {
@@ -75,7 +90,7 @@ const Topup = ({ navigation }) => {
 		} else if (res.status == 400) {
 			setAlertMessage(res.data.errors.msg)
 			setAlert(true)
-		}else {
+		} else {
 			setAlertMessage(JSON.stringify(res))
 			setAlert(true)
 		}
@@ -95,9 +110,14 @@ const Topup = ({ navigation }) => {
 					<Text>{convertRupiah(User.data.saldo)}</Text>
 				</Wrapper>
 				<View style={[styles.group]}>
-					<FloatingInput label="Nominal topup">
-						<InputCurrency value={topupValue} onChangeText={text => setTopupValue(text)} />
-					</FloatingInput>
+					{/* <FloatingInput label="Nominal topup">
+						<InputCurrency value={topupValue.toString()} onChangeText={text => setTopupValue(text)} />
+					</FloatingInput> */}
+					<FloatingInputLabelCurrency style={{ margin: 0 }}
+						value={topupValue.toString()}
+						handleChangeText={text => setTopupValue(text)}
+						label="Nominal Top Up"
+					/>
 					<Text size={10} font="Regular" color="primary">Nominal Kelipatan {convertRupiah(100000)}</Text>
 					<FlatList style={{ marginTop: 10 }} data={[100000, 200000, 500000]}
 						numColumns={3}
@@ -105,36 +125,34 @@ const Topup = ({ navigation }) => {
 						keyExtractor={(item, i) => i.toString()} />
 				</View>
 				<Text style={{ marginBottom: 10 }} font="Regular" align="center">Metode Pembayaran</Text>
-				<Accordion style={[styles.group, { padding: 0 }]} titleColor="primary" button={{ disabled: topupValue > 0 ? false : true, color: 'link' }} title={
-					<Wrapper justify="flex-start">
-						<Image size={25} style={{ marginRight: 10, }} source={require('src/assets/icons/ppob/topup/VirtualAccount.png')} />
-						<Text>Virtual Account</Text>
-					</Wrapper>
-				}>
-					<View style={{ paddingVertical: 5, backgroundColor: ColorsList.authBackground }}>
-						<FlatList
-							data={TopupMethods}
-							keyExtractor={(item, i) => i.toString()}
-							renderItem={({ item, index }) => index < 4 &&
-								<Button onPress={() => _selectTopupMethod(item, index)} style={{ marginVertical: 5 }} noBorder={!(topupMethod == item.key)} color="white">
-									<Image style={{ resizeMode: 'contain', width: 70, height: 50 }} source={item.image} />
+				{listPaymentMethod.map((item, i) => (
+					<Accordion key={i} style={[styles.group, { padding: 0 }]} titleColor="primary" button={{ disabled: topupValue > 0 ? false : true, color: 'link' }} title={
+						<Wrapper justify="flex-start">
+							<Image size={25} style={{ marginRight: 10, }} source={require('src/assets/icons/ppob/topup/VirtualAccount.png')} />
+							<Text>{item.type}</Text>
+						</Wrapper>
+					}>
+						<View style={{ paddingVertical: 5, backgroundColor: ColorsList.authBackground }}>
+							{item.data.map((list, ind) => (
+								<Button onPress={() => _selectTopupMethod(list, ind)} style={{ marginVertical: 5 }} noBorder={!(topupMethod == list.id)} color="white">
+									<Image style={{ resizeMode: 'contain', width: 70, height: 50 }} source={{uri : list.image}} />
 									<View>
-										<Text color="primary">{item.title}</Text>
-										<Text size={12}>{item.subtitle}</Text>
+										<Text color="primary">{list.name}</Text>
+										<Text size={12}>{`Total pembayaran + ${convertRupiah(topupValue + parseInt(list.fee))}`}</Text>
 									</View>
 								</Button>
-							}
-						/>
-					</View>
-				</Accordion>
-				<Accordion style={[styles.group, { padding: 0 }]} titleColor="greyFont" button={{ disabled: true, color: 'link' }} title={
+							))}
+						</View>
+					</Accordion>
+				))}
+				{/* <Accordion style={[styles.group, { padding: 0 }]} titleColor="greyFont" button={{ disabled: true, color: 'link' }} title={
 					<Wrapper justify="flex-start">
 						<Image size={25} style={{ marginRight: 10, }} source={require('src/assets/icons/ppob/topup/Rekening.png')} />
 						<Text>Transfer ke Rekening Bank</Text>
 					</Wrapper>
 				}>
 					<Text>Dolore fugiat labore reprehenderit nulla ea adipisicing velit commodo pariatur. Non est fugiat non proident adipisicing cillum officia culpa fugiat irure. Nisi dolor excepteur eiusmod occaecat proident cillum officia eiusmod elit. Consequat laboris sint ut cillum mollit ex laborum qui irure reprehenderit excepteur. Elit aliquip commodo nulla eiusmod nostrud tempor proident non ea culpa minim ea commodo voluptate. Aliquip nostrud ea officia excepteur ullamco Lorem reprehenderit exercitation veniam. Aliqua reprehenderit consequat laboris aute consectetur elit esse sit.</Text>
-				</Accordion>
+				</Accordion> */}
 			</ScrollView>
 			<Bottom>
 				<Button disabled={topupValue > 0 ? topupMethod >= 0 ? false : true : true} onPress={_handleTopUp} width="100%">TOPUP DENGAN VIRTUAL ACCOUNT</Button>
