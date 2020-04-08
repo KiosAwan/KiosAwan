19cbe19f62dcd461c171fa5128cb4388c7170b0d
slorop41314 <albertstanleyy@gmail.com>
2020-04-08 13:08:29 +0700

    Send Struk Status

--- src/screens/AuthScreen/PPOB/StatusPesanan.js
+++ src/screens/AuthScreen/PPOB/StatusPesanan.js
@@ -12,6 +12,8 @@ import { convertRupiah } from 'src/utils/authhelper';
 import moment from 'moment';
 import { getCustomer } from 'src/redux/actions/actionsCustomer';
 import { useDispatch, useSelector } from 'react-redux';
+import ViewShot from 'react-native-view-shot';
+import Screenshot from 'src/utils/screenshot';
 
 const StatusPesanan = ({ navigation }) => {
 	const dispatch = useDispatch()
@@ -40,88 +42,98 @@ const StatusPesanan = ({ navigation }) => {
 		return isCondition ? Boolean(data) : data
 	}
 
+	const _shareBill = async () => {
+		let imgPath = await Screenshot.take(viewShotRef)
+		Screenshot.share({ url: imgPath })
+	}
 	return <Container onlyTitle header={{ title: 'Status Pesanan' }}>
 		<Body>
-			{
-				_checkData('status') === 'PENDING' ?
-					<Button disabled color="warning" wrapper={{ justify: 'flex-start' }}>
-						<Icon color={ColorsList.whiteColor} name="exclamation-circle" />
-						<Text color="whiteColor" style={{ paddingHorizontal: 10 }}>Transaksi sedang diproses!</Text>
-					</Button>
-					:
-					<Button disabled color="success" wrapper={{ justify: 'flex-start' }}>
-						<Icon color={ColorsList.whiteColor} name="exclamation-circle" />
-						<Text color="whiteColor" style={{ paddingHorizontal: 10 }}>Transaksi berhasil!</Text>
-					</Button>
-			}
-			<View style={{ backgroundColor: ColorsList.whiteColor, borderRadius: 5, marginTop: 15 }}>
-				<Wrapper {...wrapper}>
-					<View>
-						<Text color="primary" size={16}>{_checkData('transaction_name').split('_').join(' ').toUpperCase()}</Text>
-						<Text>{_checkData('customerID')}</Text>
-					</View>
-					{
-						transaction && transaction.tagihan == 0 ?
-							<View /> :
-							<Text>{convertRupiah(parseInt(_checkData('total')))}</Text>
-					}
-				</Wrapper>
-				<Divider />
-				{_checkData('customer_name', true) && [
+			<ViewShot style={{backgroundColor : ColorsList.authBackground}} ref={ref => viewShotRef = ref}>
+				{
+					_checkData('status') === 'PENDING' ?
+						<Button disabled color="warning" wrapper={{ justify: 'flex-start' }}>
+							<Icon color={ColorsList.whiteColor} name="exclamation-circle" />
+							<Text color="whiteColor" style={{ paddingHorizontal: 10 }}>Transaksi sedang diproses!</Text>
+						</Button>
+						:
+						<Button disabled color="success" wrapper={{ justify: 'flex-start' }}>
+							<Icon color={ColorsList.whiteColor} name="exclamation-circle" />
+							<Text color="whiteColor" style={{ paddingHorizontal: 10 }}>Transaksi berhasil!</Text>
+						</Button>
+				}
+				<View style={{ backgroundColor: ColorsList.whiteColor, borderRadius: 5, marginTop: 15 }}>
 					<Wrapper {...wrapper}>
-						<Text>Nama Pelanggan</Text>
-						<Text>{_checkData('customer_name').trim()}</Text>
-					</Wrapper>,
+						<View>
+							<Text color="primary" size={16}>{_checkData('transaction_name').split('_').join(' ').toUpperCase()}</Text>
+							<Text>{_checkData('customerID')}</Text>
+						</View>
+						{
+							transaction && transaction.tagihan == 0 ?
+								<View /> :
+								<Text>{convertRupiah(parseInt(_checkData('total')))}</Text>
+						}
+					</Wrapper>
 					<Divider />
-				]}
-				<Wrapper {...wrapper}>
-					<Text>Tanggal Transaksi</Text>
-					<Text>{moment(_checkData('date')).format('DD MMM YYYY - hh:mm')}</Text>
-				</Wrapper>
-				<Divider />
-				{_checkData('tagihan', true) && [
+					{_checkData('customer_name', true) && [
+						<Wrapper {...wrapper}>
+							<Text>Nama Pelanggan</Text>
+							<Text>{_checkData('customer_name').trim()}</Text>
+						</Wrapper>,
+						<Divider />
+					]}
 					<Wrapper {...wrapper}>
-						<Text>Total Tagihan</Text>
-						<Text>{convertRupiah(parseInt(_checkData('tagihan')))}</Text>
-					</Wrapper>,
+						<Text>Tanggal Transaksi</Text>
+						<Text>{moment(_checkData('date')).format('DD MMM YYYY - hh:mm')}</Text>
+					</Wrapper>
 					<Divider />
-				]}
-				{_checkData('admin', true) && [
+					{_checkData('tagihan', true) && [
+						<Wrapper {...wrapper}>
+							<Text>Total Tagihan</Text>
+							<Text>{convertRupiah(parseInt(_checkData('tagihan')))}</Text>
+						</Wrapper>,
+						<Divider />
+					]}
+					{_checkData('admin', true) && [
+						<Wrapper {...wrapper}>
+							<Text>Biaya Pembayaran</Text>
+							<Text>{convertRupiah(parseInt(_checkData('admin')))}</Text>
+						</Wrapper>,
+						<Divider />
+					]}
+					{_checkData('denda', true) && [
+						<Wrapper {...wrapper}>
+							<Text>Denda</Text>
+							<Text>{convertRupiah(parseInt(_checkData('denda')))}</Text>
+						</Wrapper>,
+						<Divider />
+					]}
+					{transaction && transaction.transaction_name == "pln_prepaid" && transaction.status == "SUCCESS" && [
+						<Wrapper {...wrapper}>
+							<Text>No Token</Text>
+							<Text>{payment.token}</Text>
+						</Wrapper>,
+						<Divider />
+					]}
 					<Wrapper {...wrapper}>
-						<Text>Biaya Pembayaran</Text>
-						<Text>{convertRupiah(parseInt(_checkData('admin')))}</Text>
-					</Wrapper>,
-					<Divider />
-				]}
-				{_checkData('denda', true) && [
-					<Wrapper {...wrapper}>
-						<Text>Denda</Text>
-						<Text>{convertRupiah(parseInt(_checkData('denda')))}</Text>
-					</Wrapper>,
-					<Divider />
-				]}
-				{transaction && transaction.transaction_name == "pln_prepaid" && transaction.status == "SUCCESS" && [
-					<Wrapper {...wrapper}>
-						<Text>No Token</Text>
-						<Text>{payment.token}</Text>
-					</Wrapper>,
-					<Divider />
-				]}
-				<Wrapper {...wrapper}>
-					<Text font="ExtraBold">Total Tagihan</Text>
-					<Text font="ExtraBold">{convertRupiah(parseInt(_checkData('total')))}</Text>
-				</Wrapper>
-			</View>
+						<Text font="ExtraBold">Total Tagihan</Text>
+						<Text font="ExtraBold">{convertRupiah(parseInt(_checkData('total')))}</Text>
+					</Wrapper>
+				</View>
+			</ViewShot>
 		</Body>
 		<Footer>
-			<Wrapper style={{marginBottom : 10}} justify="space-between">
-				<Button wrapper={{ justify: 'center' }} color="white" _width="49.5%" onPress={() => navigation.navigate('/ppob')}>
+			<Wrapper style={{ marginBottom: 10 }} justify="space-between">
+				<Button wrapper={{ justify: 'center' }} color="white" _width="54.5%" onPress={() => navigation.navigate('/ppob')}>
 					<Image _style={{ marginRight: 10 }} style={{ height: 18, width: 18 }} source={require('src/assets/icons/plus-primary.png')} />
 					<Text color="primary">TAMBAH PRODUK</Text>
 				</Button>
-				<Button wrapper={{ justify: 'center' }} color="white" _width="49.5%" onPress={() => navigation.navigate('/drawer/transaction/cetakstruk', { singleData: params, type: false })}>
+				<Button wrapper={{ justify: 'center' }} color="white" _width="22%" onPress={_shareBill}>
+					<Image _style={{ marginRight: 10 }} style={{ height: 18, width: 18 }} source={require('src/assets/icons/share-primary.png')} />
+					{/* <Text color="primary">CETAK STRUK</Text> */}
+				</Button>
+				<Button wrapper={{ justify: 'center' }} color="white" _width="22%" onPress={() => navigation.navigate('/drawer/transaction/cetakstruk', { singleData: params, type: false })}>
 					<Image _style={{ marginRight: 10 }} style={{ height: 18, width: 18 }} source={require('src/assets/icons/print-primary.png')} />
-					<Text color="primary">CETAK STRUK</Text>
+					{/* <Text color="primary">CETAK STRUK</Text> */}
 				</Button>
 			</Wrapper>
 			<Button onPress={() => {
