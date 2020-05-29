9b41cba0adf3ad80a53862d53bff0fa5e3adf9d8
slorop41314 <albertstanleyy@gmail.com>
2020-05-29 13:50:19 +0700

    Paket data screen

--- src/screens/AuthScreen/PPOB/PaketData/PaketDataStyle.js
+++ src/screens/AuthScreen/PPOB/PaketData/PaketDataStyle.js
@@ -4,23 +4,22 @@ import { $Margin } from "src/utils/stylehelper"
 
 const styles = StyleSheet.create({
 	topComp: {
-		...$Margin(15, 15, 10),
+		...$Margin(0, 0, 10),
 		borderRadius: 5,
 		justifyContent: "flex-end",
 		backgroundColor: ColorsList.whiteColor
 	},
 	listPulsa: {
-		margin : 10,
+		margin: 10,
 	},
 	pulsaWrapper: {
-		justifyContent : "space-between",
-		borderRadius: 5,
 		padding: 5,
-		flex : 1,
-		borderWidth: 1,
-		borderColor: ColorsList.whiteColor,
+		borderRadius: 5,
 		backgroundColor: ColorsList.whiteColor,
-		...$Margin(0, 5, 10)
+		borderWidth: 1,
+		borderRadius: 5,
+		borderColor: ColorsList.greyAuthHard,
+		...$Margin(0, 0, 8)
 	},
 	pulsaWrapperActive: {
 		borderColor: ColorsList.primary,
@@ -29,10 +28,10 @@ const styles = StyleSheet.create({
 		...$Margin(3, 10)
 	},
 	simpan: {
-		alignSelf: "flex-end",
 		flexDirection: "row",
 		alignItems: "center",
-		marginRight: 10,
+		justifyContent: "space-between",
+		marginHorizontal: 10,
 		marginBottom: 10
 	}
 })
