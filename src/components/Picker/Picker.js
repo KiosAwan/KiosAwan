01e475068d768610a057cd10cf8517d4c7efcce5
Bambang Adhitya <bambang@kiosawan.com>
2020-06-04 14:25:16 +0700

    picker

---
+++ src/components/Picker/Picker.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Picker as PickerNB } from 'native-base';
+import { shadowStyle } from '../Input/MDInput';
+import { SizeList } from 'src/styles/size';
+import { ColorsList } from 'src/styles/colors';
+
+const Picker = props => {
+	const { data, renderItem } = props
+	return <PickerNB mode="dropdown" {...props} style={{
+		color: ColorsList.secondary
+	}}>
+		{
+			typeof renderItem == 'function' && data.rMap(v => renderItem(v))
+		}
+	</PickerNB>
+}
+
+const Item = props => <PickerNB.Item {...props} />
+
+export { Item }
+export default Picker
