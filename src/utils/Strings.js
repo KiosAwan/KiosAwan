218c73147d461636e79c72bc1001bb0776bf9dfd
slorop41314 <albertstanleyy@gmail.com>
2020-06-08 09:09:38 +0700

    Fixing bug (2,5)

--- src/utils/Strings.js
+++ src/utils/Strings.js
@@ -7,9 +7,9 @@ const Strings = {
     REGISTERPHONELABEL : 'Input your phone number',
     INTRO1TITLE : "MANAJEMEN APLIKASI KASIR\nUNTUK BISNIS MASA KINI",
     INTRO1SUBTITLE : "Nikmati kemudahan melakukan transaksi pencatatan barang. Bisnis kamu akan lebih efisien, nyaman, dan memberikan banyak keuntungan.",
-    INTRO2TITLE: "AKSES ONLINE KE SUPPLIER DENGAN HARGA LEBIH MURAH",
+    INTRO2TITLE: "AKSES ONLINE KE SUPPLIER\n DENGAN HARGA LEBIH MURAH",
     INTRO2SUBTITLE : "Dapatkan berbagai macam produk dan barang untuk kebutuhan toko kelontong secara lengkap, harga yang murah, serta pengiriman langsung ke warung. ",
-    INTRO3TITLE : "TAMBAHAN PENDAPATAN DARI PENJUALAN PRODUK DIGITAL",
+    INTRO3TITLE : "TAMBAHAN PENDAPATAN\n DARI PENJUALAN PRODUK DIGITAL",
     INTRO3SUBTITLE : "Dapatkan komisi langsung dari setiap transaksi produk digital token listrik, pulsa, pembayaran PDAM, BPJS, dll.",
     SPLASHSCREENTEXT : 'Aplikasi Wirausaha Nusantara'
 }
