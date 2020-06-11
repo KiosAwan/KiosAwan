5583a91df3b7aa23078d47d550f89415b2e9733d
slorop41314 <albertstanleyy@gmail.com>
2020-06-11 13:27:58 +0700

    Add paging to list transaksi

--- src/screens/AuthScreen/PPOB/ListTransaksi.js/index.js
+++ src/screens/AuthScreen/PPOB/ListTransaksi.js/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import { View, FlatList, ActivityIndicator } from 'react-native'
 import Container, { Body } from 'src/components/View/Container'
 import { GlobalHeader } from 'src/components/Header/Header'
@@ -23,23 +23,39 @@ const ListTransaksiPPOB = ({ navigation }) => {
 	const [filter, setFilter] = stateObject()
 	const [listTransaction, setListTransaction] = useState({})
 	const [loading, setLoading] = useState(true)
+	const [nextPage, setNextPage] = useState(1)
+	const [totalPage, setTotalPage] = useState(1)
 	const _selectDate = tanggal => {
 		let date = moment(tanggal).format('YYYY-MM-DD')
 		setFilter({ dateOpen: false, date })
 		_getData({ ...filter, date })
 	}
-	const _getData = async param => {
-		const { status, data } = await getPPOBTransactionList(param || filter)
+	const _getData = async (param, page) => {
+		const { status, data } = await getPPOBTransactionList(param || filter, page)
+		let dataResult = []
 		if (status == 200) {
-			setListTransaction(data)
+			if (nextPage == 1) {
+				dataResult = data.data
+			} else {
+				dataResult = [...listTransaction, ...data.data]
+			}
+			setNextPage(parseInt(data.current_page) + 1)
+			setTotalPage(parseInt(data.total_pages))
+			setListTransaction(dataResult)
 			setLoading(false)
 		} else {
-			setListTransaction([])
+			setListTransaction(dataResult)
 			setLoading(false)
 		}
 	}
+
+	const _addMoreData = async () => {
+		if (parseInt(nextPage) <= parseInt(totalPage)) {
+			_getData(null, nextPage)
+		}
+	}
 	useEffect(() => {
-		_getData()
+		_getData(null, 1)
 		setFilter({})
 	}, [])
 	return <Container header={{
@@ -85,34 +101,39 @@ const ListTransaksiPPOB = ({ navigation }) => {
 							listTransaction.length == 0 ?
 								<View style={{ flex: 1, alignItems: "center", justifyContent: "center", alignSelf: "center" }}>
 									<Image style={{ resizeMode: 'contain', width: 200, height: 200 }} source={require("src/assets/images/riwayat.png")} />
-									<Text font="Bold" size={16}>Tidak ada transaksi</Text>
+									<Text size={16}>Tidak ada transaksi</Text>
 								</View>
 								:
-								listTransaction.rMap((item, i) => {
-									const { transaction_name, customerID, customer_name, transaction_code, total, date } = item
-									return <Fragment>
-										<Button onPress={() => navigation.navigate(`/drawer/transaction/detail/digital`, { param: item.id })}
-											style={{ borderRadius: SizeList.borderRadius }}
-											color={["white"]}
-											key={i.toString()}
-											spaceBetween
-										>
-											<View>
-												<Wrapper justify="space-between">
-													<Text _width="65%" font="SemiBold" color="primary">{transaction_name.toString().toUpperCase().split('_').join(' ')}</Text>
-													<Text _width="35%" align="right" color="success">{convertRupiah(total)}</Text>
-												</Wrapper>
-												<Text color="greyFontHard">{customerID}</Text>
-												<Text color="greyFontHard">{customer_name}</Text>
-												<Wrapper justify="space-between" style={{ marginTop: 10 }}>
-													<Text>{transaction_code}</Text>
-													<Text>{date}</Text>
-												</Wrapper>
-											</View>
-										</Button>
-										<Divider />
-									</Fragment>
-								})
+								<FlatList
+									data={listTransaction}
+									onEndReached={_addMoreData}
+									onEndReachedThreshold={0.25}
+									renderItem={({ item }) => {
+										const { transaction_name, customerID, customer_name, transaction_code, total, date } = item
+										return <Fragment>
+											<Button onPress={() => navigation.navigate(`/drawer/transaction/detail/digital`, { param: item.id })}
+												style={{ borderRadius: SizeList.borderRadius }}
+												color={["white"]}
+												spaceBetween
+											>
+												<View>
+													<Wrapper justify="space-between">
+														<Text _width="65%" font="SemiBold" color="primary">{transaction_name.toString().toUpperCase().split('_').join(' ')}</Text>
+														<Text _width="35%" align="right" color="success">{convertRupiah(total)}</Text>
+													</Wrapper>
+													<Text color="greyFontHard">{customerID}</Text>
+													<Text color="greyFontHard">{customer_name}</Text>
+													<Wrapper justify="space-between" style={{ marginTop: 10 }}>
+														<Text>{transaction_code}</Text>
+														<Text>{date}</Text>
+													</Wrapper>
+												</View>
+											</Button>
+											<Divider />
+										</Fragment>
+									}
+									}
+								/>
 						}
 					</View>
 			}
