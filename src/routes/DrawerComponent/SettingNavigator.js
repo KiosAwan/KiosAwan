3f56d9212167e07d9c7344c16dde364d32e07be6
slorop41314 <albertstanleyy@gmail.com>
2020-06-16 10:21:06 +0700

    Remove unused setting screen

--- src/routes/DrawerComponent/SettingNavigator.js
+++ src/routes/DrawerComponent/SettingNavigator.js
@@ -3,9 +3,6 @@ import MenuSettingProfil from '../../screens/AuthScreen/Drawer/Setting/MenuSetti
 import MenuSettingUbahNoHP from '../../screens/AuthScreen/Drawer/Setting/MenuSettingUbahNoHP'
 import MenuSettingUbahPassword from '../../screens/AuthScreen/Drawer/Setting/MenuSettingUbahPassword'
 import MenuSettingLupaPIN from '../../screens/AuthScreen/Drawer/Setting/MenuSettingLupaPIN'
-import ForgotPINNewPIN from '../../screens/AuthScreen/Drawer/Setting/ForgotPINNewPIN'
-import ForgotPINOTP from '../../screens/AuthScreen/Drawer/Setting/ForgotPINOTP'
-import ChangePINNewPIN from '../../screens/AuthScreen/Drawer/Setting/ChangePINNewPIN';
 import ChangePINInputPwd from '../../screens/AuthScreen/Drawer/Setting/ChangePINInputPwd';
 import UbahEmailNewEmail from '../../screens/AuthScreen/Drawer/Setting/UbahEmailNewEmail'
 import UbahNoHPInfoScreen from '../../screens/AuthScreen/Drawer/Setting/UbahNoHPInfoScreen'
@@ -18,77 +15,59 @@ const SettingNavigator = {
     navigationOptions: {
       header: null
     }
-	},
-	'/drawer/settings/profile': {
+  },
+  '/drawer/settings/profile': {
     screen: MenuSettingProfil,
     navigationOptions: {
       header: null
     }
-	},
-  '/drawer/settings/change-email/new-email' : {
-    screen : UbahEmailNewEmail,
-    navigationOptions : {
-      header : null
+  },
+  '/drawer/settings/change-email/new-email': {
+    screen: UbahEmailNewEmail,
+    navigationOptions: {
+      header: null
     }
   },
-	'/drawer/settings/change-phone-number/change': {
+  '/drawer/settings/change-phone-number/change': {
     screen: MenuSettingUbahNoHP,
     navigationOptions: {
       header: null
     }
-	},
+  },
   '/drawer/settings/change-phone-number': {
     screen: UbahNoHPInfoScreen,
     navigationOptions: {
       header: null
     }
-	},
-	'/drawer/settings/change-password/change': {
+  },
+  '/drawer/settings/change-password/change': {
     screen: MenuSettingUbahPassword,
     navigationOptions: {
       header: null
     }
-	},
-	'/drawer/settings/lupa-pin': {
+  },
+  '/drawer/settings/lupa-pin': {
     screen: MenuSettingLupaPIN,
     navigationOptions: {
       header: null
     }
   },
-  '/drawer/settings/forgot-pin' : {
-    screen : ForgotPINOTP,
-    navigationOptions : {
-      header : null
+  '/drawer/settings/change-pin': {
+    screen: ChangePINInputPwd,
+    navigationOptions: {
+      header: null
     }
   },
-  '/drawer/settings/forgot-pin/new-pin' : {
-    screen : ForgotPINNewPIN,
-    navigationOptions : {
-      header : null
+  '/drawer/settings/perangkat': {
+    screen: PengaturanPerangkat,
+    navigationOptions: {
+      header: null
     }
   },
-  '/drawer/settings/change-pin' : {
-    screen : ChangePINInputPwd,
-    navigationOptions : {
-      header : null
-    }
-  },
-  '/drawer/settings/change-pin/new-pin' : {
-    screen : ChangePINNewPIN,
-    navigationOptions : {
-      header : null
-    }
-  },
-  '/drawer/settings/perangkat' : {
-    screen : PengaturanPerangkat,
-    navigationOptions : {
-      header : null
-    }
-  },
-  '/drawer/settings/perangkat/tambah' : {
-    screen : TambahPerangkat,
-    navigationOptions : {
-      header : null
+  '/drawer/settings/perangkat/tambah': {
+    screen: TambahPerangkat,
+    navigationOptions: {
+      header: null
     }
   },
 }
