f3e59ff4658b2177824c9daae9724831e5b7115b
slorop41314 <albertstanleyy@gmail.com>
2020-06-18 14:57:36 +0700

    Addd qr code

--- src/screens/AuthScreen/Cashier/Payment/E-WalletPayment.js
+++ src/screens/AuthScreen/Cashier/Payment/E-WalletPayment.js
@@ -4,12 +4,13 @@ import { GlobalHeader } from 'src/components/Header/Header'
 import { Text } from 'src/components/Text/CustomText'
 import { convertRupiah } from 'src/utils/authhelper'
 import { useSelector } from 'react-redux'
-import { View, StyleSheet , Image as NativeImage} from 'react-native'
-import { ColorsList } from 'src/styles/colors'
+import { View, StyleSheet, Image as NativeImage } from 'react-native'
+import { ColorsList, infoColorSetting } from 'src/styles/colors'
 import Divider from 'src/components/Row/Divider'
 import { Image } from 'src/components/CustomImage'
 import { Bottom } from 'src/components/View/Bottom'
-import { Button } from 'src/components/Button/Button'
+import { Button, Info } from 'src/components/Button/Button'
+import { SizeList } from 'src/styles/size'
 const EWalletPayment = ({ navigation }) => {
     const [amountBill, setAmount] = useState()
     const User = useSelector(state => state.User)
@@ -26,21 +27,22 @@ const EWalletPayment = ({ navigation }) => {
             <GlobalHeader title="Kode QR" onPressBack={() => navigation.goBack()} />
             <Body>
                 <View style={styles.childCont}>
-                    <Text>{User.store.name_store.toUpperCase()}</Text>
-                    <Text>{`${User.store.address_store.split('%')[0]}, ${User.store.address_store.split('%')[4]}`}</Text>
-                    <View style={[styles.qrWrap, {marginTop: 10}]}>
+                    <Text>Total belanja anda :
+                    <Text font="SemiBold" color="primary">{` ${convertRupiah(amountBill)}`}</Text>
+                    </Text>
+                    <View style={[styles.qrWrap, { marginTop: 10 }]}>
                         <View style={styles.group}>
-                            <Image size={220} source={require("src/assets/payment/AssetQR.png")} />
-                            <NativeImage style={{width : 200, height : 30, marginBottom : 20}} source={require("src/assets/payment/iconQris.png")} />
-                            <Text align="center">Silahkan scan kode QR untuk melanjutkan transaksi</Text>
-                        </View>
-                        <Divider style={{ width: "100%" }} />
-                        <View style={styles.group}>
-                            <Text>Total Transaksi</Text>
-                            <Text font="Bold" size={25} color="primary">{convertRupiah(amountBill)}</Text>
+                            <Text font="SemiBold" style={{ marginBottom: SizeList.base }} align="center">{User.store.name_store}</Text>
+                            <Text align="center">{`${User.store.address_store.split('%')[0]}`}</Text>
+                            <Divider style={{ marginVertical: SizeList.base }} />
+                            <Image style={{ alignSelf: "center" }} size={150} source={require("src/assets/payment/AssetQR.jpg")} />
+                            <NativeImage style={{ width: 150, height: 30, marginTop: SizeList.base, alignSelf: "center" }} source={require("src/assets/payment/iconQris.png")} />
                         </View>
                     </View>
                 </View>
+                <Info color={infoColorSetting} style={{ marginVertical: SizeList.padding }}>
+                    Silahkan scan kode QR untuk melanjutkan proses transaksi
+                    </Info>
             </Body>
             <Bottom>
                 <Button onPress={() => navigation.goBack()} width="100%">
@@ -55,17 +57,18 @@ export default EWalletPayment
 
 const styles = StyleSheet.create({
     childCont: {
-        alignItems: "center",
-        marginHorizontal: 25,
-        marginBottom : 90
+        // marginBottom: 90
     },
     qrWrap: {
         backgroundColor: ColorsList.whiteColor,
         width: "100%",
         alignItems: "center",
+        borderWidth: SizeList.borderWidth,
+        borderColor: ColorsList.borderColor,
+        borderRadius: SizeList.borderRadius
     },
-    group : {
-        padding : 20,
-        alignItems : "center"
+    group: {
+        padding: SizeList.padding,
+        // alignItems: "center"
     }
 })
