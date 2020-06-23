31f0b6529a6d898221fea5e1f9a31704b9f00706
Bambang Adhitya <bambang@kiosawan.com>
2020-06-23 09:24:25 +0700

    host wilayah

--- src/utils/wilayah.js
+++ src/utils/wilayah.js
@@ -2,18 +2,20 @@ import axios from "axios";
 import { HOST_URL } from "src/config";
 import { getUserToken } from "./authhelper";
 
+const HOST_WILAYAH = "http://dev.farizdotid.com"
+
 const Wilayah = {
 	Provinsi: async () => {
-		return await axios.get("http://dev.farizdotid.com/api/daerahindonesia/provinsi");
+		return await axios.get(`${HOST_WILAYAH}/api/daerahindonesia/provinsi`);
 	},
 	Kabupaten: async (idProv) => {
-		return await axios.get(`http://dev.farizdotid.com/api/daerahindonesia/provinsi/${idProv}/kabupaten`);
+		return await axios.get(`${HOST_WILAYAH}/api/daerahindonesia/provinsi/${idProv}/kabupaten`);
 	},
 	Kecamatan: async (idKab) => {
-		return await axios.get(`http://dev.farizdotid.com/api/daerahindonesia/provinsi/kabupaten/${idKab}/kecamatan`);
+		return await axios.get(`${HOST_WILAYAH}/api/daerahindonesia/provinsi/kabupaten/${idKab}/kecamatan`);
 	},
 	Desa: async (idKec) => {
-		return await axios.get(`http://dev.farizdotid.com/api/daerahindonesia/provinsi/kabupaten/kecamatan/${idKec}/desa`);
+		return await axios.get(`${HOST_WILAYAH}/api/daerahindonesia/provinsi/kabupaten/kecamatan/${idKec}/desa`);
 	},
 	SearchAddress: async pencarian => {
 		const userToken = await getUserToken()
