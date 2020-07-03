e972d91613ee7d63784ac4b934f5ac2f27f08d76
slorop41314 <albertstanleyy@gmail.com>
2020-07-03 14:26:15 +0700

    abc

--- src/components/ModalContent/ModalContent.js
+++ src/components/ModalContent/ModalContent.js
@@ -16,8 +16,8 @@ const ModalContent = (props) => {
     return <TouchableOpacity onPress={props.closeModal} style={styles.touchableStyle}>
         <View style={[styles.wrapView, props.style]}>
             <Image style={imageStyle} source={props.image} />
-            <Text align="center" style={{ marginBottom: SizeList.bodyPadding }}>{props.infoText}</Text>
-            {props.children && <View style={{ marginBottom: SizeList.bodyPadding }}>{props.children}</View>}
+            <Text align="center" style={{ marginBottom: SizeList.base }}>{props.infoText}</Text>
+            {props.children && <View style={{ marginBottom: SizeList.base }}>{props.children}</View>}
         </View>
     </TouchableOpacity>
 }
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: 'center',
         marginHorizontal: SizeList.bodyPadding * 3,
-        paddingHorizontal: SizeList.bodyPadding,
+        paddingHorizontal: SizeList.base,
         backgroundColor: 'white',
         borderRadius: SizeList.borderRadius
     }
