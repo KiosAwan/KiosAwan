efc39dcb69097dce329ebc092c2937c30cb9ecfe
Bambang Adhitya <bambang@kiosawan.com>
2020-07-08 14:15:29 +0700

    Set apiLevel & remove some features

--- android/app/src/main/java/com/awan/MainApplication.java
+++ android/app/src/main/java/com/awan/MainApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import io.sentry.RNSentryPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import cn.jystudio.bluetooth.RNBluetoothEscposPrinterPackage;
 import org.reactnative.camera.RNCameraPackage;
