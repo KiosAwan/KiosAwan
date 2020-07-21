cbd721f4611d8fca6e35c6b7165d4669cb97ac0f
Bambang Adhitya <bambang@kiosawan.com>
2020-07-21 15:39:43 +0700

    version from gradle

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,11 +1,14 @@
-export const APP_VERSION = "0.7.01.1"
+import DeviceInfo from 'react-native-device-info';
 
-export const PPOB_PRODUCT_CODE = {
-    PLN_POSTPAID : 10001,
-    PLN_PREPAID : 10002,
-    NON_TAGLIST : 10003,
-    PULSA : 10004,
-    KUOTA : 10005,
-    PDAM : 10011,
-    TELKOM_GROUP : 10006,
+const APP_VERSION = DeviceInfo.getVersion()
+
+const PPOB_PRODUCT_CODE = {
+    PLN_POSTPAID: 10001,
+    PLN_PREPAID: 10002,
+    NON_TAGLIST: 10003,
+    PULSA: 10004,
+    KUOTA: 10005,
+    PDAM: 10011,
+    TELKOM_GROUP: 10006,
 }
+export { APP_VERSION, PPOB_PRODUCT_CODE }
