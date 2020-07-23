849f3af0e523007db4295b4a64bbc12f32d3968c
slorop41314 <albertstanleyy@gmail.com>
2020-07-23 14:36:38 +0700

    url changes

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ export const HOST_URL = `${BASE_URL}/rest/api/v1`
 
 //
 const ip_url = "http://18.139.169.177"
-export const PPOB_URL = `${BASE_URL}/ppob/api/v1`
+export const PPOB_URL = `${BASE_URL}/ppob_v2/api/v1`
 
 export const HOST_IMG_URL = `${BASE_URL}/rest`
 export const DEV_URL = `${BASE_URL}/ppob/api/v1`
