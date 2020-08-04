7a7322240277b92f4fda58b38f18c2679c8e7403
slorop41314 <albertstanleyy@gmail.com>
2020-08-04 16:33:01 +0700

    topup design

--- src/components/Button/CopyButton.js
+++ src/components/Button/CopyButton.js
@@ -3,8 +3,8 @@ import { Button } from './Button';
 import { Image } from '../CustomImage';
 import { Text } from '../Text/CustomText';
 import PropTypes from 'prop-types'
-export const CopyButton = ({ onPress }) => {
-    return <Button padding={5} noBorder color="transparent" width={65} onPress={onPress}>
+export const CopyButton = ({ onPress, style }) => {
+    return <Button style={style} padding={5} noBorder color="transparent" width={65} onPress={onPress}>
         <Image source={require("src/assets/icons/copy.png")} size={15} />
         <Text size={12} color="primary" font="SemiBold">COPY</Text>
     </Button>
